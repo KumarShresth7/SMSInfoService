23fbd0177a25c0b9c160f244a5b2a71fa978793e
Kumar Shresth <kumarshresth2004@gmail.com>
2024-07-25 17:29:50 +0530

    UpdateSchema created

--- server/index.js
+++ server/index.js
@@ -15,5 +15,6 @@ mongoose.connect(process.env.MONGO_URI)
 .catch((err)=>console.log(err))
 
 app.use('/api/users',require('./routes/userRouter'))
+app.use('/api/updates',require('./routes/updateRouter'))
 
 app.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
