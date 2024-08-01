2ed750f79a38d4fa435125cb19551616697f8a98
Kumar Shresth <kumarshresth2004@gmail.com>
2024-08-01 20:52:07 +0530

    #

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Link as RouterLink, useLocation, useNavigate } from 'react-router-dom';
-import { Link as Scroll, scroller } from 'react-scroll';
+import { Link as useLocation, useNavigate } from 'react-router-dom';
+import { Link as scroller } from 'react-scroll';
 import original from "../assets/original.png";
 import './Navbar.css';
 
